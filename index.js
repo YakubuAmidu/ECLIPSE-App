c2b6e647c48bcce91b0ce14d44bfa401dd815a4c
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-24 12:12:34 -0700

    Commiting index.js file

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {  AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () =>  (
+  <View style={{ flex: 1 }}>
+  <Header headerText={'ECLIPSE!'}/>
+  <AlbumList />
+  </View>
+);
+
+AppRegistry.registerComponent('albums', () => App);
