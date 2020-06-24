22a55452534e6dca6bc25226101229d80dae0935
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-24 12:36:41 -0700

    Commiting changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,6 @@ import { Text, TouchableOpacity } from 'react-native';
 const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
 
-
   return (
     <TouchableOpacity onPress = {onPress} style={buttonStyle}>
     <Text style={textStyle}>{children}</Text>
