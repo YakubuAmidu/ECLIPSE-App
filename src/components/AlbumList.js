adae81e5cb10701dc2f577d4a530f11891c838dc
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-06-24 12:35:29 -0700

    Commiting changes

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,7 +5,6 @@ import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
   state = { albums: [] };
-  
 
   componentDidMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
